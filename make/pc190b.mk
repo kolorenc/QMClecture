8596b5eda9d732d95d59c0bca8b168a2469a3edf
Jindrich Kolorenc <kolorenc@fzu.cz>
2018-05-25 22:19:27 +0200

    Updated a makefile definition.

--- make/pc190b.mk
+++ make/pc190b.mk
@@ -4,7 +4,8 @@
 
 FC       = ifort
 FFLDCOMM = -openmp -openmp-report=2# -check all
-OPT      = -O2 -pc80 -ip -xHost -fp-model source -fp-model strict -prec-div -prec-sqrt $(FFLDCOMM)
+OPT      = -O2 -pc80 -ip -xHost -traceback# -check all# -fpe0
+OPT     += -fp-model strict -prec-div -prec-sqrt -no-ftz -assume protect_parens $(FFLDCOMM)
 # -stand f03 , -stand f95 , -stand f90 ... to check compliance with standards
 # note that -warn by default also makes those stupid files *__genmod.f90
 FFLAGS   = $(OPT) -warn -stand f03
